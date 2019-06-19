74f18f8e5f10dd648694ea48ed16aa526103c4c4
avichauhan6832 <avi.r.chauhan6832@gmail.com>
2019-06-19 18:34:53 +0530

    added some of the models and created admin

--- weworkout/catalog/models.py
+++ weworkout/catalog/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+from django.contrib.auth.models import User
+
+class Userprofile(models.Model):
+    """Model representing a User profile"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    about = models.TextField(max_length=500, blank=True)
+    followers = models.ManyToManyField(User, related_name='followers')
+    following = models.ManyToManyField(User, related_name='following')
+    workout = models.ManyToManyField('Workout', related_name='user_workout')
+
+    def __str__(self):
+        return self.Userprofile
+
+class Workout(models.Model):
+    title = models.TextField(max_length=100)
+    sets = models.IntegerField(default=0)
+    notes = models.TextField(max_length=500, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    exercise = models.ForeignKey('Exercise', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
+
+class Exercise(models.Model):
+    name = models.TextField(max_length=50)
+    description = models.TextField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
