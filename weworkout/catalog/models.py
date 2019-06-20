eba5552a2dc926ee5678433b2cc9885450427b4d
avichauhan6832 <avi.r.chauhan6832@gmail.com>
2019-06-20 18:09:07 +0530

    modified some models and added local data for development

--- weworkout/catalog/models.py
+++ weworkout/catalog/models.py
@@ -10,17 +10,18 @@ class Userprofile(models.Model):
     about = models.TextField(max_length=500, blank=True)
     followers = models.ManyToManyField(User, related_name='followers')
     following = models.ManyToManyField(User, related_name='following')
-    workout = models.ManyToManyField('Workout', related_name='user_workout')
+    workout = models.ManyToManyField('Workout', related_name='user_workout', blank=True)
 
     def __str__(self):
-        return self.Userprofile
+        return self.user.username
 
 class Workout(models.Model):
     title = models.TextField(max_length=100)
     sets = models.IntegerField(default=0)
     notes = models.TextField(max_length=500, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    exercise = models.ForeignKey('Exercise', on_delete=models.SET_NULL, null=True)
+    exercise = models.ManyToManyField('Exercise', related_name='wrokout_exercise')
+    upvotes = models.ManyToManyField(Userprofile,related_name='upvotes')
 
     def __str__(self):
         return self.title
