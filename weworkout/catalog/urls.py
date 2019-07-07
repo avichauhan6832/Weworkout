026651555851d2e8bb0ffc7fe549ef61022128d9
avichauhan6832 <avi.r.chauhan6832@gmail.com>
2019-07-07 15:38:24 +0530

    form added

--- weworkout/catalog/urls.py
+++ weworkout/catalog/urls.py
@@ -8,3 +8,9 @@ urlpatterns = [
     path('exercises/', views.ExerciseListView.as_view(), name='exercises'),
     path('exercise/<int:pk>', views.ExerciseDetailView.as_view(), name='exercise-detail'),
 ]
+
+urlpatterns += [
+    path('workout/create/', views.WorkoutCreate.as_view(), name='workout_create'),
+    path('workout/<int:pk>/update/', views.WorkoutUpdate.as_view(), name='workout_update'),
+    path('workout/<int:pk>/delete/', views.WorkoutDelete.as_view(), name='workout_delete'),
+]
--- weworkout/catalog/views.py
+++ weworkout/catalog/views.py
@@ -41,4 +41,24 @@ class ExerciseListView(generic.ListView):
 class ExerciseDetailView(generic.DetailView):
     model = Exercise
     
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
+from catalog.models import Workout
+from django.contrib.auth.mixins import PermissionRequiredMixin
+
+class WorkoutCreate(PermissionRequiredMixin, CreateView):
+    model = Workout
+    fields = '__all__'
+    initial = {'sets': 0}
+    permission_required = 'catalog.can_mark_returned'
+
+class WorkoutUpdate(PermissionRequiredMixin, UpdateView):
+    model = Workout
+    fields = ['title', 'sets', 'notes', 'exercise', 'upvotes']
+    permission_required = 'catalog.can_mark_returned'
+
+class WorkoutDelete(PermissionRequiredMixin, DeleteView):
+    model = Workout
+    success_url = reverse_lazy('workouts')
+    permission_required = 'catalog.can_mark_returned'
